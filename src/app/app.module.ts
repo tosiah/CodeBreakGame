101b62b8e5c040b7782498065b94aca8fc383e99
Antonina <tosia.h@onet.pl>
2020-06-22 18:48:47 +0200

    drag and drop handles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SymbolChoiceComponent } from './containers/symbol-choice/symbol-choice.component';
+import {GuessingListComponent} from './containers/guessing-list/guessing-list.component';
+import { SymbolPanelComponent } from './components/symbol-panel/symbol-panel.component';
+import { RestartGameComponent } from './containers/restart-game/restart-game.component';
+import {CommonModule} from '@angular/common';
+import {CodeBreakService} from './services/code-break.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SymbolChoiceComponent,
+    GuessingListComponent,
+    SymbolPanelComponent,
+    RestartGameComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
-  providers: [],
+  providers: [
+    CodeBreakService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/models/guess.interface.ts
@@ -0,0 +1,6 @@
+
+export interface Guess {
+  cipherGuessElements: number [];
+  symbolPositionMatches: number;
+  symbolMatches: number;
+}
